29b505b7d127a7ab4bba992619aaaed6ec0b2f22
Rushikesh Baraskar <3391@nucsoft.local>
2023-03-28 09:55:05 +0530

    commit after db is configured

--- springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
@@ -1,58 +1,70 @@
 package com.springrest.springrest.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
-public class CourseServiceImpl implements CourseService {
+public class CourseServiceImpl{
 	
+	@Autowired
+	private CourseDao courseDao;
 	
-	List<Course> list;
+// step-1	List<Course> list;
 	
-	public CourseServiceImpl()
-	{
-		list = new ArrayList<>();
-		list.add(new Course(145,"java course", "this is java course"));
-		list.add(new Course(132,"spring boot", "this is spring boot course"));
+//	public CourseServiceImpl()
+//	{
+//		/* step-1
+//		 * list = new ArrayList<>(); list.add(new Course(145,"java course",
+//		 * "this is java course")); list.add(new Course(132,"spring boot",
+//		 * "this is spring boot course"));
+//		 */
+//	}
+//
+//	@Override
+//	public List<Course> getCourses() {
+//		
+//		
+//		
+//	//step-1	return list; 
+//		return  courseDao.
+//	}
+	
+	@GetMapping("/course")
+	public List<Course> getAllemployees(@RequestParam Map<String, Object> params) {
+		return courseDao.findAll();
 	}
-
-	@Override
-	public List<Course> getCourses() {
-		// TODO Auto-generated method stub
-		return list;
-	}
-
-	@Override
-	public Course getCourseById(long id) {
+	
+//	@Override
+//	public Course getCourseById(long id) {
+//		
+//		/* step-1
+//		 * Course c = null; for(Course course:list) { if(course.getId() == id) { c =
+//		 * course; }
+//		 * 
+//		 * 
+//		 * }
+//		 * 
+//		 * return c;
+//		 */
 		
-		Course c = null;
-		for(Course course:list)
-		{
-			if(course.getId() == id)
-			{
-				c = course;
-			}
-			
-			
-		}
-			
-			
 		
-		// TODO Auto-generated method stub
-		return c;
-	}
-
-	@Override
-	public Course addCourse(Course course) {
-		
-		list.add(course);
-		
-		return course;
-	}
+//	}
+//
+//	@Override
+//	public Course addCourse(Course course) {
+//		
+//	//step-1	list.add(course);
+//		
+//		return course;
+//	}
 
 	
 	
