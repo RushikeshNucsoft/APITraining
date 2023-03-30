63ff2a5ce50795cbc82b9c9a509df530e6043080
Rushikesh Baraskar <3391@nucsoft.local>
2023-03-30 09:35:09 +0530

    getting error in mycontroller after running spring boot project

--- springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
@@ -12,38 +12,38 @@ import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
-public class CourseServiceImpl{
+public class CourseServiceImpl implements CourseService{
 	
 	@Autowired
 	private CourseDao courseDao;
 	
 // step-1	List<Course> list;
 	
-//	public CourseServiceImpl()
-//	{
+	public CourseServiceImpl()
+	{
 //		/* step-1
 //		 * list = new ArrayList<>(); list.add(new Course(145,"java course",
 //		 * "this is java course")); list.add(new Course(132,"spring boot",
 //		 * "this is spring boot course"));
 //		 */
-//	}
-//
-//	@Override
-//	public List<Course> getCourses() {
-//		
-//		
-//		
-//	//step-1	return list; 
-//		return  courseDao.
-//	}
+	}
+
+	@Override
+	public List<Course> getCourses() {
+		
+		
 	
-	@GetMapping("/course")
-	public List<Course> getAllemployees(@RequestParam Map<String, Object> params) {
-		return courseDao.findAll();
+	//step-1	return list; 
+		
+	
+		//step-2
+		return  courseDao.findAll();
 	}
 	
-//	@Override
-//	public Course getCourseById(long id) {
+	
+	
+	@Override
+	public Course getCourseById(long id) {
 //		
 //		/* step-1
 //		 * Course c = null; for(Course course:list) { if(course.getId() == id) { c =
@@ -55,16 +55,21 @@ public class CourseServiceImpl{
 //		 * return c;
 //		 */
 		
-		
-//	}
+		//step- 2
+		return courseDao.getOne(id);
+	}
 //
-//	@Override
-//	public Course addCourse(Course course) {
+	@Override
+	public Course addCourse(Course course) {
 //		
 //	//step-1	list.add(course);
+		
 //		
 //		return course;
-//	}
+		
+		
+		return courseDao.save(course);
+	}
 
 	
 	
