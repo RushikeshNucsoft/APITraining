c312a7e5c10d6a8157c862ea05832031dd712f31
Rushikesh Baraskar <3391@nucsoft.local>
2023-04-29 16:25:01 +0530

    rollback to step 1

--- springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
+++ springrest/src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
@@ -1,5 +1,6 @@
 package com.springrest.springrest.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,24 +9,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.springrest.springrest.dao.CourseDao;
+//import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService{
 	
-	@Autowired
-	private CourseDao courseDao;
-	
-// step-1	List<Course> list;
+	/*
+	 * @Autowired private CourseDao courseDao;
+	 */
+ List<Course> list;
 	
 	public CourseServiceImpl()
 	{
-//		/* step-1
-//		 * list = new ArrayList<>(); list.add(new Course(145,"java course",
-//		 * "this is java course")); list.add(new Course(132,"spring boot",
-//		 * "this is spring boot course"));
-//		 */
+		 
+		  list = new ArrayList<>(); list.add(new Course(145,"java course",
+		  "this is java course")); list.add(new Course(132,"spring boot",
+		  "this is spring boot course"));
+		 
 	}
 
 	@Override
@@ -33,42 +34,42 @@ public class CourseServiceImpl implements CourseService{
 		
 		
 	
-	//step-1	return list; 
+		return list; 
 		
 	
 		//step-2
-		return  courseDao.findAll();
+		//return  courseDao.findAll();
 	}
 	
 	
 	
 	@Override
 	public Course getCourseById(long id) {
-//		
-//		/* step-1
-//		 * Course c = null; for(Course course:list) { if(course.getId() == id) { c =
-//		 * course; }
-//		 * 
-//		 * 
-//		 * }
-//		 * 
-//		 * return c;
-//		 */
+		
+	
+		  Course c = null; for(Course course:list) { if(course.getId() == id) { c =
+		  course; }
+		  
+		  
+		  }
+		  
+		  return c;
+	 
 		
 		//step- 2
-		return courseDao.getOne(id);
+		//return courseDao.getOne(id);
 	}
 //
 	@Override
 	public Course addCourse(Course course) {
-//		
-//	//step-1	list.add(course);
 		
-//		
-//		return course;
+		list.add(course);
+		
+	
+		return course;
 		
 		
-		return courseDao.save(course);
+	//	return courseDao.save(course);
 	}
 
 	
